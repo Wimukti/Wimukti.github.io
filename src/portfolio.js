74ab4b3db1d59810230e79f9a050abec8a8ae7d3
Windy-GIT <wimukthi.18@cse.mrt.ac.lk>
2021-10-06 16:37:55 +0530

    Projects Added

--- src/portfolio.js
+++ src/portfolio.js
@@ -300,62 +300,6 @@ const certifications = {
       alt_name: "hackathon",
       color_code: "#E2405F",
     },
-    // {
-    //   title: 'A300: Atlas Security',
-    //   subtitle: 'MongoDB University',
-    //   logo_path: 'mongo.png',
-    //   certificate_link:
-    //     'https://drive.google.com/file/d/12u_tkvhgB-K2TIm-RDdJOwYbQ9ccqiqA/view?usp=sharing',
-    //   alt_name: 'MongoDB University',
-    //   // color_code: "#F6B808",
-    //   color_code: '#47A048',
-    // },
-    // {
-    //   title: 'Cyber Security & Cyber Forensics',
-    //   subtitle: 'Workshop at IIT Bombay',
-    //   logo_path: 'iit.png',
-    //   certificate_link:
-    //     'https://drive.google.com/file/d/12uAdjQC5LfrB1ODdxqIY181ugyyQhl-I/view?usp=sharing',
-    //   alt_name: 'Workshop',
-    //   color_code: '#2AAFED',
-    // },
-    // {
-    //   title: 'MLH Local Hack Day: Build',
-    //   subtitle: 'Major League Hacking',
-    //   logo_path: 'mlh-logo.svg',
-    //   certificate_link:
-    //     'https://drive.google.com/file/d/1ws7g2Oepu76vDFir6SvR_emFAMSkB2ZQ/view?usp=sharing',
-    //   alt_name: 'Google',
-    //   color_code: '#fe0037',
-    // },
-    // {
-    //   title: 'Hack20',
-    //   subtitle: 'Flutter International Hackathon',
-    //   logo_path: 'flutter.png',
-    //   certificate_link:
-    //     'https://drive.google.com/file/d/1JTd8pVlHbP0RVQYtfa4OZgJYGfdiCwM6/view?usp=sharing',
-    //   alt_name: 'Flutter International Hackathon',
-    //   color_code: '#2AAFED',
-    // },
-    // {
-    //   title: 'Postman Student Expert',
-    //   subtitle: 'Postman',
-    //   logo_path: 'postman.png',
-    //   certificate_link:
-    //     'https://badgr.com/public/assertions/h2qevHs4SlyoHErJQ0mn2g',
-    //   alt_name: 'Postman',
-    //   // color_code: "#f36c3d",
-    //   color_code: '#fffbf3',
-    // },
-    // color_code: "#8C151599",
-    // color_code: "#7A7A7A",
-    // color_code: "#0C9D5899",
-    // color_code: "#C5E2EE",
-    // color_code: "#ffc475",
-    // color_code: "#g",
-    // color_code: "#ffbfae",
-    // color_code: "#fffbf3",
-    // color_code: "#b190b0",
   ],
 };
 
@@ -443,19 +387,83 @@ const contactPageData = {
 
 const projects = {
   data: [
-    // {
-    //   id: '0',
-    //   name: 'AutomateInstaPyBot',
-    //   url: 'https://github.com/harikanani/AutomateInstaPyBot',
-    //   description:
-    //     'This is Instagram Bot. This will login to your Instagram account. Follow Users, Unfollow Users, Remove Profile Photo',
-    //   languages: [
-    //     {
-    //       name: 'Python',
-    //       iconifyClass: 'logos-python',
-    //     },
-    //   ],
-    // },
+    {
+      id: "0",
+      name: "AIESEC CS Portal",
+      url: "https://aiesec-village.web.app",
+      description:
+        "A complete web based responsive Human Resource Portal developed using ReactJS and Firebase. Portal includes Profiles, Blog Area, Facebook Like User Feed & Live Dashboards for Achievements.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos-firebase",
+        },
+      ],
+    },
+    {
+      id: "1",
+      name: "National Volunteer Platform ( Volunteer )",
+      url: "http://unvms.projects.uom.lk/",
+      description:
+        "The official national volunteering platform for Sri Lanka. It is a Web Application for all the volunteer organizations to post their volunteer opportunities after a systematic & secure registration.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "NodeJS",
+          iconifyClass: "logos-nodejs",
+        },
+      ],
+    },
+    {
+      id: "2",
+      name: "Cloud Hotel (Semester 5 Project)",
+      url: "https://cloud-hotel.netlify.app/",
+      description:
+        "A Web Application & a Mobile Application for large scale hotels which has multiple restaurants. Used MobX as the state management library. Backend is hosted in a Digitalocean Droplet using docker & github actions to automate the CI/CD pipeline.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "Django",
+          iconifyClass: "logos-django-icon",
+        },
+      ],
+    },
+    {
+      id: "3",
+      name: "RPG Simulator (Unity 3D)",
+      url:
+        "https://docs.google.com/presentation/d/1XCxANxSP8ct0ewVibOEKjailwsXEFg-D_7noZPEtsDA/edit#slide=id.ge1a2303847_3_1",
+      description:
+        "A RPG simulator created using Unity 3D & assets from unity assets store . The simulation has designed after a research from several other popular games.",
+      languages: [
+        {
+          name: "Unity",
+          iconifyClass: "logos-unity",
+        },
+        {
+          name: "Blender",
+          iconifyClass: "logos-blender",
+        },
+      ],
+    },
+    {
+      id: "4",
+      name: "CSE Official Web Site (Volunteer)",
+      url: "http://192.248.8.156:3000/",
+      description:
+        "The official website for Department of CSE, University of Moratuwa. Worked as a Frontend Developer.",
+      languages: [],
+    },
     // {
     //   id: '1',
     //   name: 'react-twitter-clone',
